ae0dfa4ba5776bc848eccf9d9b5bf4b62934bef0
tsmith4014 <chjthomps@gmail.com>
2022-09-24 16:14:14 -0400

    coursera numpys

---
+++ CourseraLabs/numpyArraysMatrices.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib as plt
+
+# a = [[11, 12, 13], [21, 22,23], [31, 32, 33]]
+# A = np.array(a)
+# print(A.ndim, A.shape, A.size)
+
+# print(A[0,0], A[2,2], A[1,2]) #more than one way to access the elements via bracket notation
+
+# #slicing
+
+# print(A[1, 0:2]) # row then columns
+
+# #basic operations
+
+# X = np.array([[1,0], [0,1]])
+# Y = np.array([[2,2], [2,2]])
+
+
+# # Z = X + Y
+
+# # print(Z)
+
+# # Q = Z * 2
+
+# # print(Q)
+
+# # M = X * Y
+
+# # print(M)
+
+# Multi = np.dot(X,Y)
+# print(Multi)
+
+# a = np.array([-1,1])
+# b = np.array([1,1])
+# print(np.dot(a,b))
+
